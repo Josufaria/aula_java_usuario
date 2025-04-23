198739c4d9b07402d2ced6130be03e4272a59bc8
Josufaria <josufaria18@gmail.com>
2025-04-22 22:25:10 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+let usuario = prompt ("Digite seu login:")
+let senha = prompt ("Digite a sua senha:")
+
+
+if(usuario === "Veiga" && senha === "123") {
+    alert("Login efetuado com sucesso!")
+}
+     
+if(usuario === "Estevao" && senha === "456"){
+    alert("Login efetuado com sucesso!")
+}
+    
+if(usuario === "Rony" && senha === "789") {
+    alert("Login efetuado com sucesso!")
+}
+    
+
+else {
+    alert("Usuario ou senha incorretos!")
+}
